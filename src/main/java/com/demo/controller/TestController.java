a93ce77062748c43e9cccd9a8133c3ad48e71b14
NianNian <17781665983@163.com>
2022-07-13 13:44:48 +0800

    first push

---
+++ src/main/java/com/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * create by ln on 2022/7/13
+ * file description:
+ * last update by {} on {}
+ * update description:
+ *
+ * @author ln
+ */
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String get(){
+        return "这是第一个";
+    }
+
+}
